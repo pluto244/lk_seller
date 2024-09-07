5463f47d64cb7fd648c3e907e1df0a2a046347e2
Pluto <roso3600@gmail.com>
2024-09-07 16:22:32 +0300

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// Конфигурация для Vitest
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'), 
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
   },
 });
