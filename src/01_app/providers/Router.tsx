049fcc7a9fc8b680fcd86048b7e14b025ed91eed
Pluto <roso3600@gmail.com>
2024-09-08 22:18:43 +0300

    change router for layout

--- src/01_app/providers/Router.tsx
+++ src/01_app/providers/Router.tsx
@@ -1,14 +1,23 @@
-// src/app/providers/Router.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AdvertisementsPage } from '@/02_pages/AdvertisementsPage/AdvertisementsPage';
+import { OrdersPage } from '@/02_pages/OrdersPage/OrdersPage';
+import { ResponsiveAppBar } from '@/03_widgets/ResponsiveAppBar/ResponsiveAppBar';
+import GlobalStyles from '@/06_shared/styles/GlobalStyles';
 import HomePage from '@/02_pages/HomePage';
-
+import Layout from '@/03_widgets/Layout/Layout';
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
+      <GlobalStyles />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="advertisements" element={<AdvertisementsPage />} />
+          <Route path="advertisements/:id" element={<AdvertisementsPage />} />
+          <Route path="orders" element={<OrdersPage />} />
+        </Route>
       </Routes>
     </Router>
   );
